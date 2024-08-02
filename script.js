5c43fe84adf48e952505f0828af63fd9d4a8da28
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-08-02 14:44:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function load(){
   if(screen.height > screen.width){
-    document.getElementsByTagName("nav")[0].style.height = "79px";
-    document.getElementsByTagName("nav")[0].style.fontSize = "200%";
+    document.getElementsByTagName("nav")[0].style.transform = "scale(3)";
+    document.getElementsByClassName("title")[0].style.transform = "scale(2)";
+    document.getElementsByClassName("interestbutton")[0].style.transform = "scale(2.5)";
+    document.getElementsByClassName("interestbutton")[1].style.transform = "scale(2.5)";
+    
+    btns = document.getElementsByClassName("btn");
+    for(i = 0; i < btns.length; i++){
+      btns[i].style.transform = "scale(2)";
+    }
+    
+    l = document.getElementsByClassName("card-body");
+    for(i = 0; i < l.length; i++){
+      l[i].style.transform = "scale(2)";
+    }
+    
+    headers = document.getElementsByTagName("h2");
+    for(i = 0; i < headers.length; i++){
+      headers[i].style.transform = "scale(2)";
+    }
   }
 }
