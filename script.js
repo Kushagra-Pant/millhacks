0b6b2fa0f54bf615937b6fd2d8f9d930fa90be24
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-08-02 13:56:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function load(){
+  if(screen.height > screen.width){
+    document.getElementsByTagName("nav")[0].style.height = 79px;
+    document.getElementsByTagName("nav")[0].style.fontSize = 200%;
+  }
+}
