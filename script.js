5d66fbbd491f0a0811cd6f9682cbae695f3fb31f
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-08-02 15:07:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,28 @@ function load(){
 }
 
 function scaleUp(){
+  btns = document.getElementsByTagName("navbutton");
+  for(i = 0; i < btns.length; i++){
+    btns[i].style.transform = "inline-block";
+  }
+  
   document.getElementsByTagName("nav")[0].style.transform = "scale(2)";
-    document.getElementsByClassName("title")[0].style.transform = "scale(2)";
-    document.getElementsByClassName("interestbutton")[0].style.transform = "scale(2)";
-    document.getElementsByClassName("interestbutton")[1].style.transform = "scale(2)";
-    
-    btns = document.getElementsByClassName("btn");
-    for(i = 0; i < btns.length; i++){
-      btns[i].style.transform = "scale(2)";
-    }
-    
-    l = document.getElementsByClassName("card-body");
-    for(i = 0; i < l.length; i++){
-      l[i].style.transform = "scale(2)";
-    }
-    
-    headers = document.getElementsByTagName("h2");
-    for(i = 0; i < headers.length; i++){
-      headers[i].style.transform = "scale(2)";
-    }
+  document.getElementsByClassName("title")[0].style.transform = "scale(2)";
+  document.getElementsByClassName("interestbutton")[0].style.transform = "scale(2)";
+  document.getElementsByClassName("interestbutton")[1].style.transform = "scale(2)";
+  
+  btns = document.getElementsByClassName("btn");
+  for(i = 0; i < btns.length; i++){
+    btns[i].style.transform = "scale(2)";
+  }
+  
+  l = document.getElementsByClassName("card-body");
+  for(i = 0; i < l.length; i++){
+    l[i].style.transform = "scale(2)";
+  }
+  
+  headers = document.getElementsByTagName("h2");
+  for(i = 0; i < headers.length; i++){
+    headers[i].style.transform = "scale(2)";
+  }
 }
