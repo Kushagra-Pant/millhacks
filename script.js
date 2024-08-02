a77bc3d86bd5d18cc55e4e1e61ecd78a995cf489
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-08-02 13:58:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function load(){
   if(screen.height > screen.width){
-    document.getElementsByTagName("nav")[0].style.height = 79px;
-    document.getElementsByTagName("nav")[0].style.fontSize = 200%;
+    document.getElementsByTagName("nav")[0].style.height = "79px";
+    document.getElementsByTagName("nav")[0].style.fontSize = "200%";
   }
 }
