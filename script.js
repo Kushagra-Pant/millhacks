9e7552add2f0136763ab1782e2471862601a3eeb
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-08-15 19:21:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,19 @@ function load(){
     count = Math.random() * 200 + 100
     
     for (let i = 0; i < count; i++) {
-        // Create a star element
         const star = document.createElement('div')
         star.classList.add('star')
     
-        // Random size between 1px and 4px
         size = Math.floor(Math.random() * 3) + 1
         star.style.width = `${size}px`
         star.style.height = `${size}px`
-        star.style.border = `${size / 2}px solid white` // Adjust border to create 4-sided stars
+        star.style.border = `${size / 2}px solid white`
     
-        // Random position within the container
-        star.style.top = `${Math.random() * 100}vh`
+        star.style.top = `${Math.random() * 97}vh`
         star.style.left = `${Math.random() * 100}vw`
     
-        // Random animation delay for twinkling effect
         star.style.animationDelay = `${Math.random() * 2}s`
     
-        // Append star to the container
         starContainer.appendChild(star)
     }
 }
